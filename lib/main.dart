b154fe1114b0851b2010745e35efb99c395f6494
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-03-10 18:04:57 +0000

    ADD Google Maps Flutter package and a example in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,38 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(CodingChallengeApp());
 
 const appName = "Coding Challenge";
 
-class CodingChallengeApp extends StatelessWidget {
+class CodingChallengeApp extends StatefulWidget {
+  @override
+  _CodingChallengeAppState createState() => _CodingChallengeAppState();
+}
+
+class _CodingChallengeAppState extends State<CodingChallengeApp> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  final CameraPosition _kGooglePlex = const CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962), zoom: 14.4746);
   @override
   Widget build(BuildContext context) => MaterialApp(
       title: appName,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(body: Center(child: Text("Abc"))));
+      home: Scaffold(
+          body: Center(
+              child: SizedBox(
+        width: 200,
+        height: 200,
+        child: GoogleMap(
+          initialCameraPosition: _kGooglePlex,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
+      ))));
 }
