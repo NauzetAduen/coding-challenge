94e679a983188e3b0b108b97c6b7b9fd47c181f5
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-03-15 13:18:52 +0000

    ADD getter for messages in Failures

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -5,9 +5,13 @@ abstract class Failure extends Equatable {}
 class ServerFailure extends Failure {
   @override
   List<Object> get props => [];
+
+  String get message => "There is a problem with the server.";
 }
 
 class ConnectionFailure extends Failure {
   @override
   List<Object> get props => [];
+
+  String get message => "There is a problem with your connection.";
 }
