fc381653378baebbd8cb95b60b176b105b9b842f
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-03-15 13:19:34 +0000

    ADD bloc events and states

--- lib/features/venues/presentation/bloc/venues_event.dart
+++ lib/features/venues/presentation/bloc/venues_event.dart
@@ -1,4 +1,13 @@
 part of 'venues_bloc.dart';
+
 abstract class VenuesEvent extends Equatable {
   const VenuesEvent();
+}
+
+class GetVenuesEvent extends VenuesEvent {
+  final Map<String, dynamic> queryParams;
+
+  const GetVenuesEvent({@required this.queryParams});
+  @override
+  List<Object> get props => [queryParams];
 }
--- lib/features/venues/presentation/bloc/venues_state.dart
+++ lib/features/venues/presentation/bloc/venues_state.dart
@@ -1,8 +1,31 @@
 part of 'venues_bloc.dart';
+
 abstract class VenuesState extends Equatable {
   const VenuesState();
 }
-class VenuesInitial extends VenuesState {
+
+class VenuesInitialState extends VenuesState {
   @override
   List<Object> get props => [];
+}
+
+class LoadingVenuesState extends VenuesState {
+  @override
+  List<Object> get props => [];
+}
+
+class LoadedVenuesState extends VenuesState {
+  final List<Venue> venuesList;
+
+  const LoadedVenuesState({@required this.venuesList});
+  @override
+  List<Object> get props => [venuesList];
+}
+
+class ErrorVenuesState extends VenuesState {
+  final String message;
+
+  const ErrorVenuesState({@required this.message});
+  @override
+  List<Object> get props => [message];
 }
