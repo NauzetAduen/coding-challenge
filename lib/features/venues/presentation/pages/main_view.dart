a40ce5bdd5bc76ece5d27594bc5749d233afd54d
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-03-16 15:52:44 +0000

    FIX blocProvider context not having the bloc

--- lib/features/venues/presentation/pages/main_view.dart
+++ lib/features/venues/presentation/pages/main_view.dart
@@ -1,8 +1,9 @@
-import 'package:coding_challenge/features/venues/presentation/bloc/venues_bloc.dart';
+import '../../domain/usecases/get_venues.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../injection.dart';
+import '../bloc/venues_bloc.dart';
 
 class MainView extends StatelessWidget {
   @override
@@ -13,35 +14,43 @@ class MainView extends StatelessWidget {
       ),
       body: Column(
         children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 36, vertical: 16),
-            child: SizedBox(
-              height: 60,
-              child: TextField(
-                //go vs search
-                //https://api.flutter.dev/flutter/services/TextInputAction-class.html#constants
-                textInputAction: TextInputAction.search,
-                decoration: InputDecoration(
-                    filled: true,
-                    prefixIcon: Icon(Icons.search),
-                    hintText: "Search for a place",
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(18)))),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 32),
+              child: SizedBox(
+                height: 40,
+                child: TextField(
+                  textAlignVertical: TextAlignVertical.center,
+                  //go vs search
+                  //https://api.flutter.dev/flutter/services/TextInputAction-class.html#constants
+                  textInputAction: TextInputAction.search,
+                  decoration: const InputDecoration(
+                      filled: true,
+                      prefixIcon: Icon(Icons.search),
+                      isCollapsed: true,
+                      hintText: "Search for a place",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                        Radius.circular(18),
+                      ))),
+                  onEditingComplete: () {
+                    BlocProvider.of<VenuesBloc>(context)
+                        .getVenues(const QueryParams(query: {}));
+                  },
+                ),
               ),
             ),
           ),
-          BlocProvider(
-              create: (context) => sl<VenuesBloc>(),
-              child: Expanded(
-                child: BlocBuilder<VenuesBloc, VenuesState>(
-                  builder: (context, state) {
-                    if (state is VenuesInitialState) {
-                      return const Text("INITIAL");
-                    }
-                    return const Center(child: Text("ABC"));
-                  },
-                ),
-              ))
+          Expanded(
+            child: BlocBuilder<VenuesBloc, VenuesState>(
+              builder: (context, state) {
+                if (state is VenuesInitialState) {
+                  return const Center(child: Text("What are you looking for?"));
+                }
+                return const Center(child: Text("ABC"));
+              },
+            ),
+          )
         ],
       ),
     );
