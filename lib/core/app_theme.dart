03ba153760fbea18bad09a09b5016d12159d293d
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-03-16 15:49:44 +0000

    ADD google font dependency, ADD basic Theme

---
+++ lib/core/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+ThemeData themeData = ThemeData(
+  brightness: Brightness.light,
+  primaryColor: Colors.black,
+  accentColor: Colors.yellow,
+  textTheme: GoogleFonts.montserratTextTheme(),
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'core/app_theme.dart';
 import 'core/router.dart';
+import 'features/venues/presentation/bloc/venues_bloc.dart';
 import 'injection.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   init();
   runApp(CodingChallengeApp());
 }
@@ -17,12 +21,11 @@ class CodingChallengeApp extends StatefulWidget {
 
 class _CodingChallengeAppState extends State<CodingChallengeApp> {
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: appName,
-        onGenerateRoute: CustomRouter.generateRoute,
-        initialRoute: '/',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-      );
+  Widget build(BuildContext context) => BlocProvider(
+      create: (context) => sl<VenuesBloc>(),
+      child: MaterialApp(
+          title: appName,
+          onGenerateRoute: CustomRouter.generateRoute,
+          initialRoute: '/',
+          theme: themeData));
 }
