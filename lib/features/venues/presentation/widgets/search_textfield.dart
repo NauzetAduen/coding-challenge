25b977f6e6cd4c3f1b38326d352bae42dfe23f62
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-03-19 12:08:16 +0000

    ADD query param to bloc event

--- lib/features/venues/presentation/widgets/search_textfield.dart
+++ lib/features/venues/presentation/widgets/search_textfield.dart
@@ -12,35 +12,39 @@ class SearchTextField extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 32),
-        child: SizedBox(
-          height: 40,
-          child: TextField(
-            textAlignVertical: TextAlignVertical.center,
-            //go vs search
-            //https://api.flutter.dev/flutter/services/TextInputAction-class.html#constants
-            textInputAction: TextInputAction.search,
-            decoration: const InputDecoration(
-                filled: true,
-                prefixIcon: Icon(Icons.search),
-                isCollapsed: true,
-                hintText: "Search for a place",
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                  Radius.circular(18),
-                ))),
-            onEditingComplete: () {
-              //https://flutterigniter.com/dismiss-keyboard-form-lose-focus/
-              final FocusScopeNode currentFocus = FocusScope.of(context);
+  Widget build(BuildContext context) {
+    final TextEditingController controller = TextEditingController();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 32),
+      child: SizedBox(
+        height: 40,
+        child: TextField(
+          controller: controller,
+          textAlignVertical: TextAlignVertical.center,
+          //go vs search
+          //https://api.flutter.dev/flutter/services/TextInputAction-class.html#constants
+          textInputAction: TextInputAction.search,
+          decoration: const InputDecoration(
+              filled: true,
+              prefixIcon: Icon(Icons.search),
+              isCollapsed: true,
+              hintText: "Search for a place",
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                Radius.circular(18),
+              ))),
+          onEditingComplete: () {
+            //https://flutterigniter.com/dismiss-keyboard-form-lose-focus/
+            final FocusScopeNode currentFocus = FocusScope.of(context);
 
-              if (!currentFocus.hasPrimaryFocus) currentFocus.unfocus();
-
-              BlocProvider.of<VenuesBloc>(context)
-                  .add(const GetVenuesEvent(queryParams: {}));
-            },
-          ),
+            if (!currentFocus.hasPrimaryFocus) currentFocus.unfocus();
+            BlocProvider.of<VenuesBloc>(context)
+                .add(GetVenuesEvent(queryParams: {'query': controller.text}));
+            controller.clear();
+          },
         ),
-      );
+      ),
+    );
+  }
 }
 //TODO TextEditControlled to add query to queryParams
