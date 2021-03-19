2919f325c15b774e3e584f3e464a8e3488a41e6b
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-03-19 21:34:22 +0000

    ADD fixed height to Listtile

--- lib/features/venues/presentation/widgets/venues_listview.dart
+++ lib/features/venues/presentation/widgets/venues_listview.dart
@@ -17,31 +17,40 @@ class VenuesListView extends StatelessWidget {
         //no category name
         //usually you get what it is from the name
         final Venue venue = list[index];
-        return ListTile(
-          //TODO ADD FIXED WIDTH AND HEIGHT
-          onTap: () {
-            //TODO PASS ARGUMENT ID
-            Navigator.pushNamed(context, "/detailed");
-          },
-          title: Text(venue.name),
-          subtitle: Wrap(
-            alignment: WrapAlignment.spaceBetween,
-            children: [
-              Text(venue.location.locationName),
-              Text("${venue.location.distance.toString()}m",
-                  //TODO THEME
-                  style: const TextStyle(fontSize: 10)),
-            ],
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            height: 75,
+            child: Center(
+              child: ListTile(
+                //TODO ADD FIXED WIDTH AND HEIGHT
+                onTap: () {
+                  //TODO PASS ARGUMENT ID
+                  Navigator.pushNamed(context, "/detailed");
+                },
+                title: Text(venue.name),
+                subtitle: Wrap(
+                  alignment: WrapAlignment.spaceBetween,
+                  children: [
+                    Text(venue.location.locationName),
+                    Text("${venue.location.distance.toString()}m",
+                        //TODO THEME
+                        style: const TextStyle(fontSize: 10)),
+                  ],
+                ),
+                leading: venue.category.photoUrl.isNotEmpty
+                    ? Container(
+                        //TODO use theme
+                        color: Colors.black,
+                        width: iconSize,
+                        height: iconSize,
+                        child: Center(
+                            child: Image.network(venue.category.photoUrl)))
+                    : const SizedBox(width: iconSize, height: iconSize),
+                trailing: const Icon(Icons.favorite, size: iconSize),
+              ),
+            ),
           ),
-          leading: venue.category.photoUrl.isNotEmpty
-              ? Container(
-                  //TODO use theme
-                  color: Colors.black,
-                  width: iconSize,
-                  height: iconSize,
-                  child: Center(child: Image.network(venue.category.photoUrl)))
-              : const SizedBox(width: iconSize, height: iconSize),
-          trailing: const Icon(Icons.favorite, size: iconSize),
         );
       },
     );
