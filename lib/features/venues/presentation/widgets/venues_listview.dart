9075fc0e68b3abea7253fb1c9cf9e7bd2253af57
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-03-19 12:08:39 +0000

    REFACTOR listview extracting venue

--- lib/features/venues/presentation/widgets/venues_listview.dart
+++ lib/features/venues/presentation/widgets/venues_listview.dart
@@ -12,31 +12,37 @@ class VenuesListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       physics: const BouncingScrollPhysics(),
+      itemCount: list.length,
       itemBuilder: (context, index) {
+        //no category name
+        //usually you get what it is from the name
+        final Venue venue = list[index];
         return ListTile(
-          title: Text(list[index].name),
+          onTap: () {
+            //TODO PASS ARGUMENT ID
+            Navigator.pushNamed(context, "/detailed");
+          },
+          title: Text(venue.name),
           subtitle: Wrap(
             alignment: WrapAlignment.spaceBetween,
             children: [
-              Text(list[index].location.locationName),
-              Text("${list[index].location.distance.toString()}m",
+              Text(venue.location.locationName),
+              Text("${venue.location.distance.toString()}m",
                   //TODO THEME
                   style: const TextStyle(fontSize: 10)),
             ],
           ),
-          leading: list[index].category.photoUrl.isNotEmpty
+          leading: venue.category.photoUrl.isNotEmpty
               ? Container(
                   //TODO use theme
                   color: Colors.black,
                   width: iconSize,
                   height: iconSize,
-                  child: Center(
-                      child: Image.network(list[index].category.photoUrl)))
+                  child: Center(child: Image.network(venue.category.photoUrl)))
               : const SizedBox(width: iconSize, height: iconSize),
           trailing: const Icon(Icons.favorite, size: iconSize),
         );
       },
-      itemCount: list.length,
     );
   }
 }
