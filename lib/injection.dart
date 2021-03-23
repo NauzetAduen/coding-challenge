da55e2648de0aac05b716440c9184eb54b239c10
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-03-23 16:02:04 +0000

    ADD detailsBloc and Usecase to injector

--- lib/injection.dart
+++ lib/injection.dart
@@ -2,11 +2,13 @@ import 'package:coding_challenge/core/network/network_info.dart';
 import 'package:coding_challenge/features/venues/data/datasources/venues_data_source.dart';
 import 'package:coding_challenge/features/venues/data/repositories/venues_repository_impl.dart';
 import 'package:coding_challenge/features/venues/domain/repositories/venues_repository.dart';
+import 'package:coding_challenge/features/venues/presentation/bloc/details_bloc.dart';
 import 'package:coding_challenge/features/venues/presentation/bloc/venues_bloc.dart';
 import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
+import 'features/venues/domain/usecases/get_details.dart';
 import 'features/venues/domain/usecases/get_venues.dart';
 
 final GetIt sl = GetIt.instance;
@@ -14,9 +16,11 @@ final GetIt sl = GetIt.instance;
 void init() {
   //BLOC
   sl.registerFactory(() => VenuesBloc(getVenues: sl()));
+  sl.registerFactory(() => DetailsBloc(getDetails: sl()));
 
   //USECASES
   sl.registerFactory(() => GetVenues(repository: sl()));
+  sl.registerFactory(() => GetDetails(repository: sl()));
 
   //REPOSITORIES
   sl.registerFactory<VenuesRepository>(() => VenuesRepositoryImpl(
