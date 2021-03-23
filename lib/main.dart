e0b839f1cee640190f2a5075168f7b71cece83df
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-03-23 16:40:19 +0000

    ADD multiprovider and DetailsBloc

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'core/app_theme.dart';
 import 'core/router.dart';
+import 'features/venues/presentation/bloc/details_bloc.dart';
 import 'features/venues/presentation/bloc/venues_bloc.dart';
 import 'injection.dart';
 
@@ -21,12 +22,19 @@ class CodingChallengeApp extends StatefulWidget {
 
 class _CodingChallengeAppState extends State<CodingChallengeApp> {
   @override
-  Widget build(BuildContext context) => BlocProvider(
-      create: (context) => sl<VenuesBloc>(),
-      child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: appName,
-          onGenerateRoute: CustomRouter.generateRoute,
-          initialRoute: '/',
-          theme: themeData));
+  Widget build(BuildContext context) => MultiBlocProvider(
+          providers: [
+            BlocProvider<VenuesBloc>(
+              create: (context) => sl<VenuesBloc>(),
+            ),
+            BlocProvider<DetailsBloc>(
+              create: (context) => sl<DetailsBloc>(),
+            ),
+          ],
+          child: MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: appName,
+              onGenerateRoute: CustomRouter.generateRoute,
+              initialRoute: '/',
+              theme: themeData));
 }
