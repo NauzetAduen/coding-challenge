0b01439d8f979b46261dbaf15e09c96d0d16503e
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-03-24 22:51:10 +0000

    ADD favorite datasource

---
+++ lib/features/venues/data/datasources/favorite_data_source.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract class FavoriteDataSource {
+  Set<String> changeLikeStatus(String venueID);
+}
+
+class SPFavoriteDataSourceImpl implements FavoriteDataSource {
+  final SharedPreferences sharedPreferences;
+
+  SPFavoriteDataSourceImpl({@required this.sharedPreferences});
+  @override
+  Set<String> changeLikeStatus(String venueID) {
+    final value = sharedPreferences.getString(venueID);
+    //if exist, we remove it
+    if (value != null) {
+      sharedPreferences.remove(venueID);
+    } else {
+      sharedPreferences.setString(venueID, "");
+    }
+    return sharedPreferences.getKeys();
+  }
+}
