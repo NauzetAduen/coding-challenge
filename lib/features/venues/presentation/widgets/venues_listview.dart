bdf4359d75152d926a459cf2bd6bf7b5132838b6
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-03-24 18:52:52 +0000

    FIX wrap issues with row and overflow

--- lib/features/venues/presentation/widgets/venues_listview.dart
+++ lib/features/venues/presentation/widgets/venues_listview.dart
@@ -13,51 +13,63 @@ class VenuesListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      //https://stackoverflow.com/questions/46761036/why-my-flutter-apps-listview-scroll-not-as-smooth-as-the-flutter-gallery-app/52166381
+      //we add a fixed itemExtend for performance
+      itemExtent: 85,
       physics: const BouncingScrollPhysics(),
       itemCount: list.length,
       itemBuilder: (context, index) {
         final Venue venue = list[index];
         return Padding(
           padding: const EdgeInsets.all(8.0),
-          child: SizedBox(
-            height: 75,
-            child: Center(
-              child: ListTile(
-                onTap: () {
-                  Navigator.pushNamed(context, "/detailed", arguments: venue);
-                },
-                title:
-                    CustomHero(tag: "${venue.id}name", child: Text(venue.name)),
-                subtitle: Wrap(
-                  alignment: WrapAlignment.spaceBetween,
-                  children: [
-                    CustomHero(
-                        tag: "${venue.id}location",
-                        child: Text(venue.location.locationName)),
-                    CustomHero(
-                      tag: "${venue.id}distance",
-                      child: Text("${venue.location.distance.toString()}m",
-                          //TODO THEME
-                          style: const TextStyle(fontSize: 10)),
+          child: Center(
+            child: ListTile(
+              onTap: () {
+                Navigator.pushNamed(context, "/detailed", arguments: venue);
+              },
+              title: CustomHero(
+                  tag: "${venue.id}name",
+                  child: Text(
+                    venue.name,
+                    style: Theme.of(context).textTheme.headline3,
+                  )),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                // alignment: WrapAlignment.spaceBetween,
+                children: [
+                  CustomHero(
+                      tag: "${venue.id}location",
+                      child: Text(
+                        venue.location.locationName,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.headline4,
+                      )),
+                  CustomHero(
+                    tag: "${venue.id}distance",
+                    child: Text(
+                      "${venue.location.distance.toString()}m",
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline4
+                          .copyWith(fontSize: 10),
                     ),
-                  ],
-                ),
-                leading: CustomHero(
-                  tag: "${venue.id}icon",
-                  child: venue.category.photoUrl.isNotEmpty
-                      ? Container(
-                          //TODO use theme
-                          color: Colors.black,
-                          width: iconSize,
-                          height: iconSize,
-                          child: Center(
-                              child: Image.network(venue.category.photoUrl)))
-                      : const SizedBox(width: iconSize, height: iconSize),
-                ),
-                trailing: CustomHero(
-                    tag: "${venue.id}like",
-                    child: const Icon(Icons.favorite, size: iconSize)),
+                  ),
+                ],
               ),
+              leading: CustomHero(
+                tag: "${venue.id}icon",
+                child: venue.category.photoUrl.isNotEmpty
+                    ? Container(
+                        color: Colors.black,
+                        width: iconSize,
+                        height: iconSize,
+                        child: Center(
+                            child: Image.network(venue.category.photoUrl)))
+                    : const SizedBox(width: iconSize, height: iconSize),
+              ),
+              trailing: CustomHero(
+                  tag: "${venue.id}like",
+                  child: const Icon(Icons.favorite, size: iconSize)),
             ),
           ),
         );
