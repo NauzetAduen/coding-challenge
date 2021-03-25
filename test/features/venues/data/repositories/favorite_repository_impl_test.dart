0dec7da95074c615374de3f01d6aa11636e1c7a8
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-03-25 12:22:05 +0000

    ADD tests for repository impl

---
+++ test/features/venues/data/repositories/favorite_repository_impl_test.dart
@@ -0,0 +1,21 @@
+import 'package:coding_challenge/features/venues/data/datasources/favorite_data_source.dart';
+import 'package:coding_challenge/features/venues/domain/repositories/favorite_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockFavoriteDataSource extends Mock implements SPFavoriteDataSourceImpl {}
+
+void main() {
+  final MockFavoriteDataSource mockFavoriteDataSource =
+      MockFavoriteDataSource();
+  final FavoriteRepositoryImpl favoriteRepositoryImpl =
+      FavoriteRepositoryImpl(favoriteDataSource: mockFavoriteDataSource);
+  group('description', () {
+    test('should return a Set<String> when calling datasource', () {
+      when(mockFavoriteDataSource.toogleLikeStatus(any)).thenAnswer((_) => {});
+
+      final result = favoriteRepositoryImpl.toogleFavoriteStatus("venueID");
+      expect(result, isA<Set<String>>());
+    });
+  });
+}
