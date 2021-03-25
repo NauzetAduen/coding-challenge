daddc412165e8f5921be993a7ab0a0daa00d17e4
Nauzet Hernández <nauzet.aduen@gmail.com>
2021-03-25 12:27:20 +0000

    ADD toogleFavStatus usecase test

---
+++ test/features/venues/domain/usecases/toogle_favorite_status_test.dart
@@ -0,0 +1,22 @@
+import 'package:coding_challenge/features/venues/domain/repositories/favorite_repository.dart';
+import 'package:coding_challenge/features/venues/domain/usecases/toogle_favorite_status.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockFavoriteRepositoryImpl extends Mock
+    implements FavoriteRepositoryImpl {}
+
+void main() {
+  final MockFavoriteRepositoryImpl mockFavoriteRepositoryImpl =
+      MockFavoriteRepositoryImpl();
+  final ToogleFavoriteStatus usecase =
+      ToogleFavoriteStatus(favoriteRepository: mockFavoriteRepositoryImpl);
+
+  test('should get a Set<String> from the repository', () async {
+    when(mockFavoriteRepositoryImpl.toogleFavoriteStatus("venueID"))
+        .thenAnswer((_) => {});
+
+    final result = usecase(venueID: "venueID");
+    expect(result, isA<Set<String>>());
+  });
+}
